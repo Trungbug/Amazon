0645fe55d862d42915e53263a01f8538df103d6f
Trungbug <baotrung2032608@gmail.com>
2024-09-19 22:52:39 +0700

    tao search

--- src/Components/NavBar/Navigation.js
+++ src/Components/NavBar/Navigation.js
@@ -12,6 +12,18 @@ class NavBar extends Component {
             < div className="navbar_component" >
 
                 <div className="navbar_logo"></div>
+                <div className="navbar_locator">
+                    <div className="navbar_locatorImages"></div>
+                    <div className="navbar_location">VietNam</div>
+                </div>
+                <div className="navbar_searchcomponet">
+                    <div>
+                    </div>
+                    <div><input type="text" className="navbar_searchbox"></input></div>
+                    <div className="navbar_searchboxdiv">
+                        <div className="navbar_searchicon"></div>
+                    </div>
+                </div>
             </div >
         );
     }
